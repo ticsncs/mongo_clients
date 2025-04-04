5e616fa830db26fe356723b8191593866c705a93
leninfabricio29 <leninfabricio7@gmail.com>
2025-04-04 16:27:52 -0500

    Update client.ts

--- src/models/client.ts
+++ src/models/client.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ClienteSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
-  telefono: { type: String, required: true , default: "000000000" },
+  telefono: { type: String, required: false , default: "000000000" },
   contratos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Contrato" }],
 }, { timestamps: true });
 
