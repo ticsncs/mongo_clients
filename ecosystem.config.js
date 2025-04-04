77c46923368ea09a66c1fdc0d39ffed8c014e910
serverproyectos <byron.herrera@unl.edu.ec>
2025-04-04 09:46:24 -0500

    cambios

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
     env: {
       NODE_ENV: "production",
       PORT: 3040,
+      HOST: "0.0.0.0",
       MONGO_URI: "mongodb://userncs2025:ticsncs2025@190.96.96.20:27017/bd_clientes?authSource=admin"
     }
   }]
